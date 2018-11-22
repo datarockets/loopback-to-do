cb49c79afac2cbac08bccca396b0aba96d185623
DDKatch <daniil.field@gmail.com>
2018-11-22 16:27:08 +0300

    Fix client style code

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
-  state = {
-    response: ''
-  };
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
             Learn React
-          </a>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
-      </div>
-    );
-  }
+        </a>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
