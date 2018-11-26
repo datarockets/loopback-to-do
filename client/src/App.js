21a2839eb865bc5c4d6e1fa58897546ecce1e189
Nikita <n.sakov@datarockets.com>
2018-11-26 12:23:23 +0300

    Moved header js and css to separate files

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import './App.css'
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
       <Header />
-    </div>
-  )
-}
-
-function Header() {
-  return (
-    <div className="Header">
-      <h1>TODO list</h1>
     </div>
   )
 }
