5171920f2e8cd6e1bf8a388c4aec2c7a224b1f24
Nikita <n.sakov@datarockets.com>
2018-11-30 17:14:08 +0300

    Installed jasmine

---
+++ server/spec/cards.spec.js
@@ -0,0 +1,7 @@
+describe("Post request /api/v1/cards", function() {
+  it("check if text is 'Hello World'", function(done) {
+    let text = "Hello World";
+    expect(text).toBe("Hello World");
+    done();
+  });
+});
