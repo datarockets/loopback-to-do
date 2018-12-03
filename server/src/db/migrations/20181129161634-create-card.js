4ef30d25867cd3bab2dab8d8a1583b9a7baaa869
Nikita <n.sakov@datarockets.com>
2018-12-03 10:03:56 +0300

    Old syntax of migrations was backed and migrations added to ignore

--- server/src/db/migrations/20181129161634-create-card.js
+++ server/src/db/migrations/20181129161634-create-card.js
@@ -1,22 +1,27 @@
+'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('Cards', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER,
-    },
-    description: {
-      type: Sequelize.STRING,
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-    },
-  }),
-  down: (queryInterface) => { queryInterface.dropTable('Cards') },
-}
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Cards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Cards');
+  }
+};
