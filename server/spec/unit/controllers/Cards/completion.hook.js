c3df613cba828633bfaa146a90c9e85171a91cdf
Nikita <n.sakov@datarockets.com>
2018-12-13 18:25:57 +0300

    Added delete completion info to api-doc

--- server/spec/unit/controllers/Cards/completion.hook.js
+++ server/spec/unit/controllers/Cards/completion.hook.js
@@ -3,3 +3,7 @@ import { before } from 'hooks'
 before("Card Completion Collection > POST", (transaction) => {
   transaction.skip = true
 })
+
+before("Card Completion Collection > DELETE", (transaction) => {
+  transaction.skip = true
+})
