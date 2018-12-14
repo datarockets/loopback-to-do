e6b9b896a53c08a493e1e3ef5bde2bd197192b3e
Nikita <n.sakov@datarockets.com>
2018-12-14 13:57:22 +0300

    Implemented create method in completion controller

--- server/src/controllers/v1/cards/completion.js
+++ server/src/controllers/v1/cards/completion.js
@@ -2,7 +2,14 @@ const { Card } = require('../../../models')
 
 module.exports = {
   create: (req, res) => {
-    res.send('test')
+    Card.findById(req.params.id)
+      .then((card) => {
+        card.update({
+          completedAt: Date.now(),
+        }).then(() => { res.status(201).send({ completedAt: card.completedAt }) })
+          .catch((error) => { res.status(400).send(error) })
+      })
+      .catch((error) => { res.status(400).send(error) })
   },
   destroy: (req, res) => {
     Card.findById(req.params.id)
