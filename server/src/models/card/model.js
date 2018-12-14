435c041e503057263d5c8810b213dae680d06c54
Nikita <n.sakov@datarockets.com>
2018-12-14 13:40:53 +0300

    Added field completedAt to cardmodel

--- server/src/models/card/model.js
+++ server/src/models/card/model.js
@@ -1,6 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Card = sequelize.define('Card', {
     description: { type: DataTypes.STRING, allowNull: false },
+    completedAt: { type: DataTypes.DATE, allowNull: true },
   }, {})
 
   return Card
