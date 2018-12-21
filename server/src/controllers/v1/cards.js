dc0442cc3a75909c3ccd2c2ce291fc1bf3af4678
Nikita <n.sakov@datarockets.com>
2018-12-21 12:34:32 +0300

    Created first version of destroy controller

--- server/src/controllers/v1/cards.js
+++ server/src/controllers/v1/cards.js
@@ -10,7 +10,14 @@ module.exports = {
       .then((card) => { res.status(201).send(card) })
       .catch((error) => { res.status(400).send(error) })
   },
-
+  destroy: (req, res) => {
+    Card
+      .destroy({
+        where: { id: req.params.id },
+      })
+      .then(() => { res.status(204).send() })
+      .catch((error) => { res.status(400).send(error) })
+  },
   getAll: (req, res) => {
     Card.findAll({ raw: true })
       .then((cards) => { res.status(200).send(cards) })
