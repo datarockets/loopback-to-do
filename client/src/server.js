1e34cc46fcd56354b7c1f588cb4b667913a2eb9e
Nikita <n.sakov@datarockets.com>
2019-01-08 20:53:16 +0300

    Refactored cardTag route in server.js

--- client/src/server.js
+++ client/src/server.js
@@ -5,7 +5,7 @@ const routes = {
   card: id => `${BASE_URL}/cards/${id}`,
   cardCompletion: id => `${routes.card(id)}/completion`,
   cardTags: id => `${routes.card(id)}/tags`,
-  cardTag: (cardId, tagId) => `${routes.card(cardId)}/tags/${tagId}`,
+  cardTag: (cardId, tagId) => `${routes.cardTags(cardId)}/${tagId}`,
 }
 
 export default routes
